e5f187661bcd58415945ad1a9b3e856a6d9f9265
Nimish <nimish.ie19@nsut.ac.in>
2023-11-07 22:40:18 +0530

    origin

--- Array ADT/set_oper.cpp
+++ Array ADT/set_oper.cpp
@@ -18,6 +18,95 @@ void Display(struct Array arr){
 //Union
 struct Array* Union(struct Array *arr1, struct Array *arr2)
 {
+    int i,j,k;
+    i=j=k=0;
+
+    struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+    while(i<arr1->length && j<arr2->length)
+    {
+        
+        if(arr1->A[i] < arr2->A[j])
+            arr3->A[k++]=arr1->A[i++];    //it is doing k++ after storing in k, just a shortcut way to write
+        else if(arr2->A[j]<arr1->A[i])
+            arr3->A[k++]=arr2->A[j++];
+        else
+        {
+            arr3->A[k++]=arr1->A[i++];
+            j++;
+        }
+    }
+    for(;i<arr1->length;i++)
+        arr3->A[k++]=arr1->A[i];
+    for(;j<arr2->length;j++)
+        arr3->A[k++]=arr2->A[j];
+
+    arr3->length=k;
+    arr3->size=10;
+
+    return arr3;
+    
+}
+
+//intersection function
+struct Array* Intersection(struct Array *arr1, struct Array *arr2)
+{
+    int i,j,k;
+    i=j=k=0;
+
+    struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+    while(i<arr1->length && j<arr2->length)
+    {
+        
+        if(arr1->A[i]<arr2->A[j])
+            i++;   
+        else if(arr2->A[j]<arr1->A[i])
+            j++;
+        else
+        {
+            arr3->A[k++]=arr1->A[i++];
+            
+        }
+    }
+   
+    arr3->length=k;
+    arr3->size=10;
+
+    return arr3;
+    
+}
+
+//Difference
+struct Array* Difference(struct Array *arr1, struct Array *arr2)  //array1-array2
+{
+    int i,j,k;
+    i=j=k=0;
+
+    struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+    while(i<arr1->length && j<arr2->length)
+    {
+        
+        if(arr1->A[i] < arr2->A[j])
+        {
+            arr3->A[k++]=arr1->A[i++];          
+            
+        }
+        else if (arr2->A[j]<arr1->A[i])
+    {
+            j++;
+            
+        }
+        else{
+            i++;
+        }
+    }
+    for(;i<arr1->length;i++)
+        arr3->A[k++]=arr1->A[i];
+    
+
+    arr3->length=k;
+    arr3->size=10;
+
+    return arr3;
     
 }
 
@@ -26,10 +115,14 @@ struct Array* Union(struct Array *arr1, struct Array *arr2)
 int main()
 {
     struct Array arr1={{2,6,10,15,25},10,5};
-    struct Array arr2={{3,4,7,18,20},10,5};
+    struct Array arr2={{3,4,15,18,20},10,5};
     struct Array *arr3;
 
     arr3=Union(&arr1,&arr2);
+    Display(*arr3);
+    arr3=Intersection(&arr1,&arr2);
+    Display(*arr3);
+    arr3=Difference(&arr1,&arr2);
     Display(*arr3);
 
     return 0;
